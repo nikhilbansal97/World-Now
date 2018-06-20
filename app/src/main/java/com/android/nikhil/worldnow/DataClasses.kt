dfdf733fc74fb1b08184e2730b2699635a8f924e
Nikhil Bansal <nikhil97.nb@gmail.com>
2018-06-20 17:33:12 +0530

    Update: Removed redundant fields from data class

--- app/src/main/java/com/android/nikhil/worldnow/DataClasses.kt
+++ app/src/main/java/com/android/nikhil/worldnow/DataClasses.kt
@@ -8,32 +8,12 @@ data class MainResponse (
 )
 
 data class NewsResponse (
-        @SerializedName("status")
-        val status: String,
-        @SerializedName("userTier")
-        val userTier: String,
-        @SerializedName("total")
-        val total: Int,
-        @SerializedName("startIndex")
-        val startIndex: Int,
-        @SerializedName("pageSize")
-        val pageSize: Int,
-        @SerializedName("currentPage")
-        val currentPage: Int,
-        @SerializedName("pages")
-        val pages: Int,
-        @SerializedName("orderBy")
-        val orderBy: String,
         @SerializedName("results")
         val results: ArrayList<Result>)
 
 data class Result (
         @SerializedName("id")
         val id: String,
-        @SerializedName("type")
-        val type: String,
-        @SerializedName("sectionId")
-        val sectionId: String,
         @SerializedName("sectionName")
         val sectionName: String,
         @SerializedName("webPublicationDate")
@@ -41,12 +21,4 @@ data class Result (
         @SerializedName("webTitle")
         val webTitle: String,
         @SerializedName("webUrl")
-        val webUrl: String,
-        @SerializedName("apiUrl")
-        val apiUrl: String,
-        @SerializedName("isHosted")
-        val isHosted: Boolean,
-        @SerializedName("pillarId")
-        val pillarId: String,
-        @SerializedName("pillarName")
-        val pillarName: String)
+        val webUrl: String)
