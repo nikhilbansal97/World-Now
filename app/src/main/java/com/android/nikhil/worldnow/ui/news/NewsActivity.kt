f4be9c3e7cd40e84a71c8dc1d80e37166d695d2d
Naledi Madlopha <naledi.madlopha@gmail.com>
2018-10-05 00:31:22 +0200

    Refactor the NewsRecyclerAdapter
    * Remove the implementation of the NewsItemsClickListener from the
    NewsActivity. Creating a new interface to handle the news item click is
    an overkill, there is already a View.OnClickListener interface which the
    NewsViewHolder can be implement.
    * Implement the View.OnclickListener in the NewsViewHolder, to contain
    the news list item code within the adapter's view holder and therefore
    adhering to the principle of Separation of Concern.
    * Implement the LayoutContainer in the NewsViewHolder, invoking
    findViewById() can be slow, especially in case of huge view hierarchies,
    so Android Extensions tries to minimize findViewById() calls by caching
    views in containers.

--- app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsActivity.kt
+++ app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsActivity.kt
@@ -1,18 +1,15 @@
 package com.android.nikhil.worldnow.ui.news
 
 import android.arch.lifecycle.Observer
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.View.GONE
+import android.view.View
 import com.android.nikhil.worldnow.R
 import com.android.nikhil.worldnow.ui.base.BaseActivity
-import com.android.nikhil.worldnow.utils.NewsItemClickListener
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class NewsActivity : BaseActivity<NewsViewModal>(), NewsItemClickListener {
+class NewsActivity : BaseActivity<NewsViewModal>() {
 
     private lateinit var adapter: NewsRecyclerAdapter
     @Inject lateinit var mViewModel: NewsViewModal
@@ -21,26 +18,22 @@ class NewsActivity : BaseActivity<NewsViewModal>(), NewsItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = NewsRecyclerAdapter(this, this, ArrayList())
+        adapter = NewsRecyclerAdapter(this, ArrayList())
         newsRecyclerView.adapter = adapter
 
         mViewModel.getNewsData().observe(this, Observer { list ->
             run {
+                newsListProgressBar.visibility = View.GONE
+
                 if (list != null) {
                     adapter.swapNewsData(list)
                 } else {
-                    Log.d(NewsActivity::class.java.simpleName, "Updated list is null")
+                    Log.d(NewsActivity::class.java.simpleName, "Updated resultsList is null")
                 }
             }
         })
 
         mViewModel.getNews()
-    }
-
-    override fun onNewsClicked(url: String) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(url)
-        startActivity(intent)
     }
 
     override fun getViewModelClass(): Class<NewsViewModal> = NewsViewModal::class.java
--- app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsRecyclerAdapter.kt
+++ app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsRecyclerAdapter.kt
@@ -1,52 +1,77 @@
 package com.android.nikhil.worldnow.ui.news
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.android.nikhil.worldnow.R
 import com.android.nikhil.worldnow.ui.news.NewsRecyclerAdapter.NewsViewHolder
-import com.android.nikhil.worldnow.utils.NewsItemClickListener
 import com.android.nikhil.worldnow.utils.Result
-import kotlinx.android.synthetic.main.list_item.view.item_date
-import kotlinx.android.synthetic.main.list_item.view.item_section
-import kotlinx.android.synthetic.main.list_item.view.item_title
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.news_article_card.*
 
-class NewsRecyclerAdapter(var context: Context,var listener: NewsItemClickListener, var list: ArrayList<Result>?):
+class NewsRecyclerAdapter(var context: Context, var resultsList: ArrayList<Result>?):
         RecyclerView.Adapter<NewsViewHolder>() {
 
     override fun getItemCount(): Int {
-        return if (list != null) list!!.size else 0
+        return if (resultsList != null) resultsList!!.size else 0
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        if (list != null) {
-            val currentNews = list!![position]
-            holder.apply {
-                textViewTitle.text = currentNews.webTitle
-                textViewSection.text = currentNews.sectionName
-                textViewDate.text = currentNews.webPublicationDate
-                view.setOnClickListener { _ -> listener.onNewsClicked(currentNews.webUrl) }
-            }
-        }
+        if (resultsList != null) holder.bind(resultsList!!.get(position))
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-       return NewsViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item, parent, false))
+       return NewsViewHolder(LayoutInflater.from(context).inflate(R.layout.news_article_card, parent, false))
     }
 
     fun swapNewsData(updatedList: ArrayList<Result>) {
-        if (list == null) list = ArrayList<Result>()
-        else list?.clear()
-        list?.addAll(updatedList)
+        if (resultsList == null) {
+            resultsList = ArrayList()
+        } else {
+            resultsList?.clear()
+        }
+
+        resultsList?.addAll(updatedList)
         notifyDataSetChanged()
     }
 
-    inner class NewsViewHolder(var view: View): RecyclerView.ViewHolder(view) {
-        var textViewTitle = view.item_title as TextView
-        var textViewSection = view.item_section as TextView
-        var textViewDate = view.item_date as TextView
+    inner class NewsViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), View.OnClickListener, LayoutContainer {
+
+        lateinit var result: Result
+
+        fun bind(result: Result) {
+            this.result = result
+
+            /*
+                TODO
+                To include an article image, the service query to The Guardian API has to
+                include a parameter called 'show-fields' with the value 'thumbnail',
+                e.g. show-fields=thumbnail. This will return a response with results that contain
+                the fields which in turn contains the thumbnail of the article. Use this link with
+                your API key to see the response: http://content.guardianapis.com/search?api-key=YOUR_API_KEY_HERE&show-fields=thumbnail
+
+                Refactoring the service to include the thumbnail will allow us to get the thumbnail
+                in the result data class and use it in the app using the line below:
+
+                Glide.with(context).load(result.thumbnail()).into(articleCardImageView)
+            */
+
+            articleCardTitleTextView.text = result.webTitle
+            articleCardDateTextView.text = result.webPublicationDate
+
+            containerView.setOnClickListener(this)
+
+        }
+
+        override fun onClick(v: View?) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(result.webUrl)
+            context.startActivity(intent)
+        }
     }
+
 }
