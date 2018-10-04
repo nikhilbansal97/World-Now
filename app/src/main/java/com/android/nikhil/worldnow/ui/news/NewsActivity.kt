2c5ca3eaa9d2c1de5d925acd86a798850de41035
Naledi Madlopha <naledi.madlopha@gmail.com>
2018-10-04 22:21:18 +0200

    Implement News Article List Item Card
    * Implement a new news list item
    * Set the recycler view layout manager to StaggeredGridLayoutManager
    in the xml instead of the setting it in the code. This minimizes the
    code in the NewsActivity.kt file
    * Add a progress bar in the main activity which is displayed by default,
    it should be hidden when the list of news items has been loaded onto the
    screen
    * Refactor the news activity to make it easy to understand what is
    happening

--- app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsActivity.kt
+++ app/src/main/java/com/android/nikhil/worldnow/ui/news/NewsActivity.kt
@@ -4,20 +4,16 @@ import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.View.GONE
 import com.android.nikhil.worldnow.R
-import com.android.nikhil.worldnow.utils.NewsItemClickListener
 import com.android.nikhil.worldnow.ui.base.BaseActivity
-import com.android.nikhil.worldnow.utils.Result
-import kotlinx.android.synthetic.main.activity_main.newsRecyclerView
+import com.android.nikhil.worldnow.utils.NewsItemClickListener
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class NewsActivity : BaseActivity<NewsViewModal>(), NewsItemClickListener {
 
-    override fun getViewModelClass(): Class<NewsViewModal> = NewsViewModal::class.java
-
-    private val TAG = NewsActivity::class.java.simpleName
     private lateinit var adapter: NewsRecyclerAdapter
     @Inject lateinit var mViewModel: NewsViewModal
 
@@ -25,26 +21,28 @@ class NewsActivity : BaseActivity<NewsViewModal>(), NewsItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get the ViewHolder for this activity
-        adapter = NewsRecyclerAdapter(this, this, ArrayList<Result>())
+        adapter = NewsRecyclerAdapter(this, this, ArrayList())
         newsRecyclerView.adapter = adapter
-        newsRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Observe the MutableLiveData in the ViewHolder.
         mViewModel.getNewsData().observe(this, Observer { list ->
             run {
-                if (list != null) adapter.swapNewsData(list)
-                else Log.d(TAG, "Updated list is null")
+                if (list != null) {
+                    adapter.swapNewsData(list)
+                } else {
+                    Log.d(NewsActivity::class.java.simpleName, "Updated list is null")
+                }
             }
         })
 
         mViewModel.getNews()
     }
 
-    // This function is called when a news item is clicked.
     override fun onNewsClicked(url: String) {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         startActivity(intent)
     }
+
+    override fun getViewModelClass(): Class<NewsViewModal> = NewsViewModal::class.java
+
 }
